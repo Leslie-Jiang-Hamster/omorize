720fc8c27e8c6cbfbcfb313dbedb448f44bf322f
Leslie_Jiang <Leslie_Jiang@proton.me>
2024-06-05 00:40:14 +0800

    fix: init DBUS connection

--- util.py
+++ util.py
@@ -6,6 +6,8 @@ def pick_one(wallpaper_dir: str) -> str:
   return picked
 
 def set_bg_to(bg_uri: str) -> None:
-  if os.system(f'gsettings set org.gnome.desktop.background picture-uri "file://{bg_uri}"'):
+  desktop_initer = 'export DISPLAY=":0"; '
+  desktop_initer += 'export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus";'
+  if os.system(f'{desktop_initer} gsettings set org.gnome.desktop.background picture-uri "file://{bg_uri}"'):
     print('set_bg_to: system error')
     exit(-1)
